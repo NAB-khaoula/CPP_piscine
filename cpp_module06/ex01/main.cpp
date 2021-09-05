51d293319fd989d63d740a73a741991cb10c95dd
Khaoula Naboussi <k12.naboussi@gmail.com>
2021-09-05 08:53:24 +0100

    started ex01

---
+++ cpp_module06/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+typedef struct  s_data{
+	int     _int;
+	float   _float;
+	double	_double;
+}               Data;
+
+uintptr_t	serialize(Data *ptr){
+
+}
+
+int main(){
+	Data		*ptr;
+	uintptr_t	raw;
+}
