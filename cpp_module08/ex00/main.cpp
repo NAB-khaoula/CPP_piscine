f099b0204db2bb4f785f772aabe6cdf1fbf71816
Khaoula Naboussi <k12.naboussi@gmail.com>
2021-09-15 14:28:57 +0100

    day08

--- cpp_module08/ex00/main.cpp
+++ cpp_module08/ex00/main.cpp
@@ -9,7 +9,7 @@ int main()
     std::vector<int> v;
     v.push_back(1337); 
     v.push_back(42); 
-    v.push_back(19); 
+    v.push_back(19);
     for (size_t i = 0; i < v.size(); i++)
         std::cout << v[i] << std::endl;
     easyfind(v, 42);
